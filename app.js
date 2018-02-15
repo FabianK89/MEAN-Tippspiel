8ccc3ae0a5a8917ef0eb95ac9b46a4f502de21c2
FabianK <fabian.kiesl@gmail.com>
2018-02-16 00:24:14 +0100

    MEAN Stack Front To Back Tutorial Parts 1 - 3
    Followed the instructions of the tutorial parts 1 to 3

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to database
+mongoose.connect(config.database);
+
+// On connection
+mongoose.connection.on('connected', () => {
+    console.log('connected to database ' + config.database);
+});
+
+// On error
+mongoose.connection.on('error', (err) =>
+{
+    console.log('Database error: ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint')
+});
+
+// Start server
+app.listen(port, () => {
+    console.log('Server startet on port ' + port)
+});
