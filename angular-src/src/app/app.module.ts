ade14f39d7afa136a527b995db6a4547c31f2967
FabianK89 <fabian.kiesl@gmail.com>
2018-02-19 16:29:05 +0100

    MEAN Stack Front To Back [Part 6]
    Implemented register component, a field and email validaten and flash messages

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,6 +11,8 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
+import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -32,9 +34,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService, FlashMessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
